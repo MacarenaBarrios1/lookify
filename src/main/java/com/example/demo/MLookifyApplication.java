241a790159a04abcd6353af4442c18e420fbb87f
Macarena Barrios <macarena.barrios08@gmail.com>
2023-04-30 17:02:26 -0400

    firts commit

---
+++ src/main/java/com/example/demo/MLookifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MLookifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MLookifyApplication.class, args);
+	}
+
+}
